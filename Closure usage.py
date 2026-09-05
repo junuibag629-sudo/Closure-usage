825b9e90c87f51c60d841c281cebf9ce4c3b0809
junuibag629 <junuibag629@gmail.com>
2026-09-05 14:36:02 +0900

    3수정 내용

--- Closure usage.py
+++ Closure usage.py
@@ -1,5 +1,5 @@
-def countdown(n):
-        i = n
+def countdown(n):    
+    i = n
     def count():
         nonlocal i
         i -= 1
@@ -10,4 +10,4 @@ n = int(input())
 
 c = countdown(n)
 for i in range(n):
-    print(c(), end= '')
+    print(c(), end= ' ')
